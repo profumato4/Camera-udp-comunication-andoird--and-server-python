cc25a494782187241e23a93f93be1477ad712845
profumato4 <Leonardobelli2006@gmail.com>
2023-12-24 15:23:04 +0100

    update

--- app/build/generated/source/buildConfig/androidTest/debug/com/example/cameraxexample/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/cameraxexample/test/BuildConfig.java
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.example.cameraxexample.test;
+package com.example.camerax.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
